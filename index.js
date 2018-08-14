ff74b198f6f2f33c237a2f19cafabeac76b9438d
shrpne <shrpne@gmail.com>
2018-08-14 21:07:15 +0300

    add slug

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 module.exports = [
     {
+        slug: 'intro',
         title: 'Intro',
         url: 'https://www.minter.network/',
     },
     {
+        slug: 'network',
         title: 'Network',
         url: 'https://about.minter.network/',
     },
     {
+        slug: 'console',
         title: 'Console',
         url: 'https://testnet.console.minter.network/',
     },
     {
+        slug: 'status',
         title: 'Status',
         url: 'https://status.minter.network/testnet/',
     },
     {
-        title: 'Intro',
+        slug: 'explorer',
+        title: 'Explorer',
         url: 'https://testnet.explorer.minter.network/',
     },
     {
-        title: 'APU & SDK',
+        slug: 'api',
+        title: 'API & SDK',
         url: 'https://github.com/MinterTeam',
     },
     {
+        slug: 'docs',
         title: 'Docs',
         url: 'https://minter-go-node.readthedocs.io/en/latest/',
     },
