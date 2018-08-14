895c92bbd67d2c35189af3a6f69c8478ced5df42
shrpne <shrpne@gmail.com>
2018-08-14 21:00:10 +0300

    initial

---
+++ index.js
@@ -0,0 +1,30 @@
+module.exports = [
+    {
+        title: 'Intro',
+        url: 'https://www.minter.network/',
+    },
+    {
+        title: 'Network',
+        url: 'https://about.minter.network/',
+    },
+    {
+        title: 'Console',
+        url: 'https://testnet.console.minter.network/',
+    },
+    {
+        title: 'Status',
+        url: 'https://status.minter.network/testnet/',
+    },
+    {
+        title: 'Intro',
+        url: 'https://testnet.explorer.minter.network/',
+    },
+    {
+        title: 'APU & SDK',
+        url: 'https://github.com/MinterTeam',
+    },
+    {
+        title: 'Docs',
+        url: 'https://minter-go-node.readthedocs.io/en/latest/',
+    },
+];
