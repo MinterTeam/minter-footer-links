304c536dbbf9c58975bebe255c2c009396cb767c
shrpne <shrpne@gmail.com>
2018-09-04 19:14:21 +0300

    localize title

--- index.js
+++ index.js
@@ -1,37 +1,58 @@
 module.exports = [
     {
         slug: 'intro',
-        title: 'Intro',
+        title: {
+            en: 'Intro',
+            ru: 'Интро',
+        },
         url: 'https://www.minter.network/',
     },
     {
         slug: 'network',
-        title: 'Network',
+        title: {
+            en: 'Network',
+            ru: 'Сеть',
+        },
         url: 'https://about.minter.network/',
     },
     {
         slug: 'console',
-        title: 'Console',
+        title: {
+            en: 'Console',
+            ru: 'Консоль',
+        },
         url: 'https://testnet.console.minter.network/',
     },
     {
         slug: 'status',
-        title: 'Status',
+        title: {
+            en: 'Status',
+            ru: 'Статус',
+        },
         url: 'https://status.minter.network/testnet/',
     },
     {
         slug: 'explorer',
-        title: 'Explorer',
+        title: {
+            en: 'Explorer',
+            ru: 'Эксплорер',
+        },
         url: 'https://testnet.explorer.minter.network/',
     },
     {
         slug: 'api',
-        title: 'API & SDK',
+        title: {
+            en: 'API & SDK',
+            ru: 'API и SDK',
+        },
         url: 'https://github.com/MinterTeam',
     },
     {
         slug: 'docs',
-        title: 'Docs',
+        title: {
+            en: 'Docs',
+            ru: 'Документация',
+        },
         url: 'https://minter-go-node.readthedocs.io/en/latest/',
     },
 ];
